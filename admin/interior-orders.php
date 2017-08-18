f54000cc22e833aba36977211d0d77c4723db96a
shrinath598 <srinath.chinchole@qleverlabs.com>
2017-08-18 20:43:27 +0530

    date issues

--- admin/interior-orders.php
+++ admin/interior-orders.php
@@ -71,18 +71,14 @@ $db = new PDO("mysql:host=".DBHOST.";port=3306;dbname=".DBNAME, DBUSER, DBPASS);
 	<title>Dashboard - <?php echo SITE_TITLE;?></title>
 	<?php include_once $_SERVER['DOCUMENT_ROOT']."/includes/css.php";?>
 	<link rel="stylesheet" type="text/css" href="/css/home.css?<?php echo RANDOM_NO;?>">
-		  <script language="JavaScript" type="text/javascript">
-  function delpost(id, title)
-  {
-	  if (confirm("Are you sure you want to delete "))
-	  {
-	  	window.location.href = 'interior-orders.php?delpost=' + id;
-	  }
-  }
-  </script>
 
-     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.css" />
+
 
 
 </head>
@@ -91,12 +87,26 @@ $db = new PDO("mysql:host=".DBHOST.";port=3306;dbname=".DBNAME, DBUSER, DBPASS);
 		<section class="main-content">
 			<?php include_once $_SERVER['DOCUMENT_ROOT']."/includes/navs.php";?>
 			<div class="wrapper container-fluid">
+
 				<div class="fit-main-content">
+
 					<div class="table-responsive">
+                            <div class="row">
+     <div class="input-daterange">
+      <div class="col-md-4">
+       <input type="text" name="start_date" id="start_date" class="form-control" />
+      </div>
+      <div class="col-md-4">
+       <input type="text" name="end_date" id="end_date" class="form-control" />
+      </div>      
+     </div>
+     <div class="col-md-4">
+      <input type="button" name="search" id="search" value="Search" class="btn btn-info" />
+     </div>
+    </div>
 
 
-
- <table class="table table-bordered" id="example" >
+ <table class="table table-bordered" id="order_data" >
 	                <thead>
              <th>Action</th>
                                 <th>Date</th>                        
@@ -198,14 +208,70 @@ $db = new PDO("mysql:host=".DBHOST.";port=3306;dbname=".DBNAME, DBUSER, DBPASS);
 
 
 	<?php include_once $_SERVER['DOCUMENT_ROOT']."/includes/scripts.php";?>
-		          <script type="text/javascript" src="//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
-      <script type="text/javascript" src="//cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+	<!-- 	          <script type="text/javascript" src="//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+      <script type="text/javascript" src="//cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script> -->
    
 
-	    <script  type="text/javascript" >
+<!-- 	    <script  type="text/javascript" >
         $(document).ready(function() {
     $('#example').DataTable();
 } );
-    </script>
+    </script> -->
+ <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.js"></script>
+<script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+ 
+ $('.input-daterange').datepicker({
+  todayBtn:'linked',
+  format: "yyyy-mm-dd",
+  autoclose: true
+ });
+
+ fetch_data('no');
+
+ function fetch_data(is_date_search, start_date='', end_date='')
+ {
+  var dataTable = $('#order_data').DataTable({
+   "processing" : true,
+   "serverSide" : true,
+   "order" : [],
+   "ajax" : {
+    url:"fetch3.php",
+    type:"POST",
+    data:{
+     is_date_search:is_date_search, start_date:start_date, end_date:end_date
+    }
+   }
+  });
+ }
+
+ $('#search').click(function(){
+  var start_date = $('#start_date').val();
+  var end_date = $('#end_date').val();
+  if(start_date != '' && end_date !='')
+  {
+   $('#order_data').DataTable().destroy();
+   fetch_data('yes', start_date, end_date);
+  }
+  else
+  {
+   alert("Both Date is Required");
+  }
+ }); 
+ 
+});
+</script>
+          <script  type="text/javascript">
+  function delpost(id, title)
+  {
+      if (confirm("Are you sure you want to delete "))
+      {
+        window.location.href = 'interior-orders.php?delpost=' + id;
+      }
+  }
+  </script>
 </body>
 </html>
