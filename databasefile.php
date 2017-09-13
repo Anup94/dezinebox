69db448fe5bbbc7148065e66833a052ff78f3d6c
shrinath598 <srinath.chinchole@qleverlabs.com>
2017-09-13 11:30:51 +0530

    pulling dezine

---
+++ databasefile.php
@@ -0,0 +1,80 @@
+<!doctype>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Anything</title>
+</head>
+<body>
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "xls-trial";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+
+require_once "EXCEL/classes/PHPExcel.php";
+
+	$tmpfname = "SECOND.csv" ;
+	$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+	$excelObj = $excelReader->load($tmpfname);
+	$worksheet = $excelObj->getActiveSheet();
+	$lastrow = $worksheet->getHighestRow();
+
+	echo "<table>";
+	for($row = 1 ; $row <= $lastrow; $row++) {
+		echo "<tr><td>";
+	$name = $worksheet->getCell('A'.$row)->getValue();
+echo $name;
+		echo "</td><td>";
+	$highschool = $worksheet->getCell('B'.$row)->getValue();
+	echo $highschool;
+
+
+		echo "</td><td>";
+$state = $worksheet->getCell('C'.$row)->getValue();
+	echo $state;
+
+
+echo "</td><td>";
+$club =  $worksheet->getCell('D'.$row)->getValue();
+echo $club;
+
+
+		echo "</td><td>";
+	$college =  $worksheet->getCell('E'.$row)->getValue();
+	echo   $college ;
+
+
+		echo "</td><td>";
+$position =  $worksheet->getCell('D'.$row)->getValue();
+echo $position;
+	
+
+
+	
+
+	
+
+	$sql = "INSERT INTO commitment (name ,highschool , state ,club ,college , position,  division , year , gender) values('$name' , 
+	'$highschool' , '$state','$club', '$college' , '$position' , '1'  ,  '2019' , 'boys')  ";
+	if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+}
+	echo "</table>";
+?>
+</body>
+</html>
